cab493db0f314ca81a3b8e2b647a98d955e06c24
cianoruanaidh <cianor.social@gmail.com>
2019-02-12 13:38:56 -0500

    Game logic updated: can no longer click when game is over, clicking on boot pauses ability, cannot double click an element, can restart a game

--- js/script.js
+++ js/script.js
@@ -1,92 +1,145 @@
 
-// SCOREING FUNCTIONS
-let score = 0;
+// set up game variables
+// let score = 0;
+let initialBait = 0;
+let baitCount = 0;
+let level = 'easy'
 let gameOn = true;
+let gamesPlayed = 0;
 
 // if bait is clicked, increment score by 1
-$('body').on('click', '.bug', function () {
-    if (gameOn === true) {
-        score++;
-        console.log(score);
-        $('.score').html(score);
+$('body').on('click', '.bait', function () {
+    if (gameOn === true ){
+        $(this).removeClass('not-clicked')
+        $(this).addClass("clicked");    
+        baitCount --;
+        // score++;
+        $('.score').html(baitCount);
     }
-    else {
-        console.log("GAME IS OBVVERRRR");
-    }
+    // // $(this).hide();
+    // if (gameOn === true) {
+    // }
+    // else {
+    //     console.log("GAME IS OBVVERRRR");
+    // }
 });
 
 // if non-bait is clicked, decrease score by 1
 $('body').on('click', '.boot', function () {
-    if (score > 0) {
-        score--;
+    if (gameOn === false) {
+        console.log("gameOn not true");
+    } else {
+        gameOn = false;
+        $('.game-area').addClass("paused"); 
+        setTimeout( function(){ 
+            gameOn = true;
+            $('.game-area').removeClass("paused");
+          }  , 2000 );    
     }
-    console.log(score);
-    $('.score').html(score);
 });
 
 // SET UP PLAYING BOARD FUNCTION
 // declare initial function
-let bugHtml = '<div class="card bug"><img src="assets/worm.svg" alt="worm"></div>';
-let bootHtml = '<div class="card boot"><img src="assets/boot.svg" alt="worm"></div>';
+let bait1 = "worm.svg";
+let bait2 = "caterpillar.svg";
+let boot1 = "boot.svg";
+let boot2 = "boot-2.svg";
 let gameBoardHTML = '';
 
 // function to create board
 function createGameBoard(count) {
     for (let i=0; i<count; i++){
         let randoNum = Math.floor(Math.random() * 100);
-        console.log(randoNum);
-        if (randoNum < 40) {
-            gameBoardHTML += bugHtml;
+        // console.log(randoNum);
+        if (randoNum < 20) {
+            baitCount ++ ;
+            gameBoardHTML += `<div class="card bait not-clicked"><img src="assets/${bait1}" alt="worm"></div>`;
+        } else if (randoNum < 40) {
+            baitCount ++ ;
+            gameBoardHTML += `<div class="card bait not-clicked"><img src="assets/${bait2}" alt="worm"></div>`;
+        } else if (randoNum < 70) {
+            gameBoardHTML += `<div class="card boot not-clicked"><img src="assets/${boot1}" alt="worm"></div>`;
         } else {
-            gameBoardHTML += bootHtml;
-        } 
+            gameBoardHTML += `<div class="card boot not-clicked"><img src="assets/${boot2}" alt="worm"></div>`;
+        }
     }
+    initialBait = baitCount;
 }
 
+// function to change game difficulty setting
+$('.level').on('click', function(){
+    level = $(this).html().toLowerCase();
+});
 
+// function to control game length and logic
 function playGame() {
-    let gameClock = 34;
-    $('.game-area .holder h2').html(3);  
+
+    // creat board based on level of difficulty
+    if (level === 'easy'){
+        createGameBoard(32);
+    } else if (level === 'medium') {
+        createGameBoard(64);
+    } else {
+        createGameBoard(96);
+    }
+
+    // set gameLength and gameClock
+    let gameLength = 10;
+    let gameClock = gameLength + 4;
+
+    // start countdown from 3
+    $('.game-area .holder h2').html("READY?");  
+
     let countInterval = setInterval(timer, 800);
     function timer(){
-        if (gameClock > 31) {
-            console.log(gameClock-31);
+        if (gameClock > (gameLength + 1)) {
+            // continue countdown to game
+            $('.game-area .holder h2').html(gameClock-(gameLength + 1));
             gameClock = gameClock -1;
-            $('.game-area .holder h2').html(gameClock-31);
-        } else if (gameClock === 31){
-            console.log("START!");
-            gameClock = gameClock -1;
+
+        } else if (gameClock === (gameLength + 1)){
+            // tell player the game is starting & how many bugs they have to get
             $('.game-area .holder h2').html("START!");
-        } else if (gameClock === 30) {
+            $('.score').html(baitCount);
+            gameClock = gameClock -1;
+
+        } else if (gameClock === gameLength) {
+            // populate game-area board with cards & set gam timer
             $('.game-area').html(gameBoardHTML);
             $('.timer').html(gameClock);
             gameClock = gameClock -1;
+
         } else if (gameClock > 0) {
+            // count down game timer
             $('.timer').html(gameClock);
             gameClock = gameClock -1;
+
         } else {
-            console.log("GAME OVER!");
+            // time is up, game is over
             $('.timer').html("Times UP!");
+            console.log(baitCount);
+            if (baitCount > 0) {
+                $('.result').html(`You missed ${baitCount} clicks! Try again?`);
+            } else {
+                $('.result').html(`You got all ${initialBait} clicks! YOU WIN!`);
+            }
             gameOn = false;
             clearInterval(countInterval);
+            $('.start').html("Restart");
         }
     }
 }
 
+// FUBCTION TO START GAME ON CLICK
 $('.start').on('click', function(){
-    playGame();
-});
-
-// depending on level: create different sized board
-$('.level').on('click', function(){
-    level = $(this).html().toLowerCase();
-    easyBoardHtml = "";
-    if (level === 'easy'){
-        createGameBoard(16);
-    } else if (level === 'medium') {
-        createGameBoard(32);
+    if (gamesPlayed = 0 ){
+        playGame();
     } else {
-        createGameBoard(48);
+        gameOn = true;
+        $('.game-area').html('<div class="holder"><h2></h2></div>');
+        playGame();
     }
 });
+
+
 
